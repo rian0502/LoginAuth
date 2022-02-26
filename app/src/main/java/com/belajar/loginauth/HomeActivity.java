a64c0b24ae22db88ac29622a747f1101c5a66d96
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-02-26 17:26:35 +0700

    update androidx

--- app/src/main/java/com/belajar/loginauth/HomeActivity.java
+++ app/src/main/java/com/belajar/loginauth/HomeActivity.java
@@ -1,6 +1,6 @@
 package com.belajar.loginauth;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
